0aa123fce5025d6f13369fe2f53af4c2c8a3b88a
dungx <dungx@DESKTOP-NAQPEID>
2016-10-24 09:46:03 +0700

    Add project files.

---
+++ FTPDemo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPDemo
+{
+    public class Class1
+    {
+    }
+}
